58aaa1a538f3a51cce5a875743f616831864ffd6
ThuyHien261195 <thuyhien261195@gmail.com>
2017-10-10 17:15:26 +0700

    Format directory to store View, Presenter, Acitivy of Main Screen, remove function getContext and rename function show logged in view in MainView

--- app/src/main/java/com/example/thuyhien/simplelogin/ui/presenter/MainPresenter.java
+++ app/src/main/java/com/example/thuyhien/simplelogin/ui/presenter/MainPresenter.java
@@ -1,4 +1,4 @@
-package com.example.thuyhien.simplelogin.ui.main;
+package com.example.thuyhien.simplelogin.ui.presenter;
 
 /**
  * Created by thuyhien on 10/9/17.
---
+++ app/src/main/java/com/example/thuyhien/simplelogin/ui/view/MainView.java
@@ -0,0 +1,9 @@
+package com.example.thuyhien.simplelogin.ui.view;
+
+/**
+ * Created by thuyhien on 10/9/17.
+ */
+
+public interface MainView {
+    void showLoggedInView();
+}
