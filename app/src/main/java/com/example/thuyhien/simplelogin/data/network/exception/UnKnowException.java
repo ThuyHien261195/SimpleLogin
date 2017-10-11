502a82035934921b177a288ecf502e6f15574e9e
ThuyHien261195 <thuyhien261195@gmail.com>
2017-10-11 13:52:03 +0700

    Add UnKnowException

---
+++ app/src/main/java/com/example/thuyhien/simplelogin/data/network/exception/UnKnowException.java
@@ -0,0 +1,11 @@
+package com.example.thuyhien.simplelogin.data.network.exception;
+
+/**
+ * Created by thuyhien on 10/11/17.
+ */
+
+public class UnKnowException extends Exception{
+    public UnKnowException() {
+        super();
+    }
+}
