e56480fe381c590d96b10d1e3bd25010cd52454c
ThuyHien261195 <thuyhien261195@gmail.com>
2017-10-12 22:41:41 +0700

    Add method getData

--- app/src/main/java/com/example/thuyhien/simplelogin/data/network/retrofit/DataEndpointInterface.java
+++ app/src/main/java/com/example/thuyhien/simplelogin/data/network/retrofit/DataEndpointInterface.java
@@ -1,12 +1,15 @@
 package com.example.thuyhien.simplelogin.data.network.retrofit;
 
+import com.example.thuyhien.simplelogin.model.FeedPost;
 import com.example.thuyhien.simplelogin.model.Page;
 
 import java.util.List;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
+import retrofit2.http.Url;
 
 /**
  * Created by thuyhien on 10/11/17.
@@ -16,4 +19,10 @@ public interface DataEndpointInterface {
 
     @GET("api/v1/pages.json")
     Call<List<Page>> getPageList(@Query("platform") String platform);
+
+    @GET()
+    Call<List<FeedPost>> getFeedList(@Url String feedUrl);
+
+    @GET()
+    Call<ResponseBody> getImagePost(@Url String imageUrl);
 }
