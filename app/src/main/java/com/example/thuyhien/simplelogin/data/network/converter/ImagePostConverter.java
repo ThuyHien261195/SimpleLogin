3a8c98fd84018280c0a8f82d9e5cbd9847d98409
ThuyHien261195 <thuyhien261195@gmail.com>
2017-10-13 20:44:10 +0700

    Define global default width and height of poster

--- app/src/main/java/com/example/thuyhien/simplelogin/data/network/converter/ImagePostConverter.java
+++ app/src/main/java/com/example/thuyhien/simplelogin/data/network/converter/ImagePostConverter.java
@@ -1,6 +1,7 @@
 package com.example.thuyhien.simplelogin.data.network.converter;
 
 import com.example.thuyhien.simplelogin.model.ImagePost;
+import com.example.thuyhien.simplelogin.utils.ImageUtils;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -21,8 +22,8 @@ public class ImagePostConverter extends BaseDeserializer<ImagePost> {
             imagePost = new ImagePost();
             imagePost.setImageUrl(getStringValue(jsonObject.get("url"), ""));
             imagePost.setImageTitle(getStringValue(jsonObject.get("title"), ""));
-            imagePost.setWidth(getIntValue(jsonObject.get("width"), 100));
-            imagePost.setHeight(getIntValue(jsonObject.get("height"), 100));
+            imagePost.setWidth(getIntValue(jsonObject.get("width"), ImageUtils.REQUEST_WIDTH));
+            imagePost.setHeight(getIntValue(jsonObject.get("height"), ImageUtils.REQUEST_HEIGHT));
         }
         return imagePost;
     }
