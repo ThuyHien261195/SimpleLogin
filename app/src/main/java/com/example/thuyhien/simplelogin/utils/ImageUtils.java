ad8acbba2ef6ec93e7584f09607fa1fceaea46fb
ThuyHien261195 <thuyhien261195@gmail.com>
2017-10-13 20:43:39 +0700

    Update code ImageUtils class

--- app/src/main/java/com/example/thuyhien/simplelogin/utils/ImageUtils.java
+++ app/src/main/java/com/example/thuyhien/simplelogin/utils/ImageUtils.java
@@ -1,8 +1,37 @@
 package com.example.thuyhien.simplelogin.utils;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.io.InputStream;
+
 /**
  * Created by thuyhien on 10/13/17.
  */
 
 public class ImageUtils {
+    public static final int REQUEST_WIDTH = 100;
+    public static final int REQUEST_HEIGHT = 120;
+
+    public static Bitmap decodeBitMapFromInputStream(InputStream inputStream, int outWidth, int outHeight) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = calculateInSampleSize(outWidth, outHeight);
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeStream(inputStream, null, options);
+    }
+
+    private static int calculateInSampleSize(int outWidth, int outHeight) {
+        int inSampleSize = 1;
+
+        if (outHeight > REQUEST_HEIGHT || outWidth > REQUEST_WIDTH) {
+            int halfHeight = outHeight / 2;
+            int halfWidth = outWidth / 2;
+
+            while ((halfHeight / inSampleSize) >= REQUEST_HEIGHT
+                    && (halfWidth / inSampleSize) >= REQUEST_WIDTH) {
+                inSampleSize *= 2;
+            }
+        }
+        return inSampleSize;
+    }
 }
