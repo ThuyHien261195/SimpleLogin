ff07fd6fe1d2987d831661404b7fe960b53d84c1
ThuyHien261195 <thuyhien261195@gmail.com>
2017-10-13 14:30:52 +0700

    Add ImagePostConverter class

---
+++ app/src/main/java/com/example/thuyhien/simplelogin/data/network/converter/ImagePostConverter.java
@@ -0,0 +1,27 @@
+package com.example.thuyhien.simplelogin.data.network.converter;
+
+import com.example.thuyhien.simplelogin.model.ImagePost;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+
+import java.lang.reflect.Type;
+
+/**
+ * Created by thuyhien on 10/13/17.
+ */
+
+public class ImagePostConverter extends BaseDeserializer<ImagePost> {
+    @Override
+    public ImagePost deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        ImagePost imagePost = null;
+        if (checkValidJsonObject(json)) {
+            JsonObject jsonObject = json.getAsJsonObject();
+            imagePost = new ImagePost();
+            imagePost.setImageUrl(getStringValue(jsonObject.get("url"), ""));
+            imagePost.setImageTitle(getStringValue(jsonObject.get("title"), ""));
+        }
+        return imagePost;
+    }
+}
