66e01fdea8edebbe424aa5175d765d2cd4f4d420
ThuyHien261195 <thuyhien261195@gmail.com>
2017-10-16 16:46:08 +0700

    Format code, import library path

--- app/src/main/java/com/example/thuyhien/simplelogin/data/interactor/LoadDataInteractor.java
+++ app/src/main/java/com/example/thuyhien/simplelogin/data/interactor/LoadDataInteractor.java
@@ -3,16 +3,14 @@ package com.example.thuyhien.simplelogin.data.interactor;
 import com.example.thuyhien.simplelogin.data.interactor.listener.OnLoadFeedListener;
 import com.example.thuyhien.simplelogin.data.interactor.listener.OnLoadImageListener;
 import com.example.thuyhien.simplelogin.data.interactor.listener.OnLoadPageListListener;
-import com.example.thuyhien.simplelogin.model.ImagePost;
+import com.example.thuyhien.simplelogin.model.MediaImage;
 
 /**
  * Created by thuyhien on 10/12/17.
  */
 
 public interface LoadDataInteractor {
-    void getPostList(final OnLoadPageListListener listener);
+    void getPageList(final OnLoadPageListListener listener);
 
-    void getFeedList(String feedUrl, final OnLoadFeedListener listener);
-
-    void getPoster(ImagePost imagePost, final OnLoadImageListener listener);
+    void getPoster(MediaImage imagePost, final OnLoadImageListener listener);
 }
