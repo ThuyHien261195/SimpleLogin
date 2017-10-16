4611d0e981d90dd7b0b6a13d968021c16b01a2a5
ThuyHien261195 <thuyhien261195@gmail.com>
2017-10-16 16:48:58 +0700

    Add hide and showloading call back to view, update getFeedList by asynctask

--- app/src/main/java/com/example/thuyhien/simplelogin/data/network/converter/FeedPostConverter.java
+++ app/src/main/java/com/example/thuyhien/simplelogin/data/network/converter/FeedPostConverter.java
@@ -1,9 +1,8 @@
 package com.example.thuyhien.simplelogin.data.network.converter;
 
-import com.example.thuyhien.simplelogin.model.FeedPost;
-import com.example.thuyhien.simplelogin.model.ImagePost;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.example.thuyhien.simplelogin.FoxApplication;
+import com.example.thuyhien.simplelogin.model.MediaFeed;
+import com.example.thuyhien.simplelogin.model.MediaImage;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonElement;
@@ -12,6 +11,7 @@ import com.google.gson.JsonParseException;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -20,48 +20,46 @@ import java.util.Map;
  * Created by thuyhien on 10/11/17.
  */
 
-public class FeedPostConverter extends BaseDeserializer<FeedPost> {
+public class FeedPostConverter extends BaseDeserializer<MediaFeed> {
     @Override
-    public FeedPost deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-        FeedPost feedPost = null;
+    public MediaFeed deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        MediaFeed feedPost = null;
         if (checkValidJsonObject(json)) {
-            feedPost = new FeedPost();
+            feedPost = new MediaFeed();
             JsonObject jsonObject = json.getAsJsonObject();
             feedPost.setId(getStringValue(jsonObject.get("id"), ""));
             feedPost.setGuid(getStringValue(jsonObject.get("guid"), ""));
             feedPost.setTitle(getStringValue(jsonObject.get("title"), ""));
             feedPost.setDescription(getStringValue(jsonObject.get("description"), ""));
-
-            ImagePost imagePost = getImagePost(jsonObject.get("thumbnails"));
-            if (imagePost != null) {
-                feedPost.setPoster(imagePost);
-            }
+            List<MediaImage> thumbnails = getImagePost(jsonObject.get("thumbnails"));
+            feedPost.setThumbnails(thumbnails);
         }
         return feedPost;
     }
 
-    private ImagePost getImagePost(JsonElement jsonElement) {
-        GsonBuilder gsonBuilder = new GsonBuilder()
-                .registerTypeAdapter(ImagePost.class, new ImagePostConverter());
-        Gson gson = gsonBuilder.create();
+    private List<MediaImage> getImagePost(JsonElement jsonElement) {
         Type thumbnailType;
         if (checkValidJsonObject(jsonElement)) {
-            thumbnailType = new TypeToken<HashMap<String, ImagePost>>() {
+            thumbnailType = new TypeToken<HashMap<String, MediaImage>>() {
             }.getType();
-            HashMap<String, ImagePost> thumbnails = gson.fromJson(jsonElement, thumbnailType);
+            HashMap<String, MediaImage> thumbnails = FoxApplication.getInstance()
+                    .getDataGson()
+                    .fromJson(jsonElement, thumbnailType);
             if (thumbnails != null && thumbnails.size() != 0) {
-                Map.Entry<String, ImagePost> thumbnailsEntry = thumbnails.entrySet().iterator().next();
-                return thumbnailsEntry.getValue();
+                return new ArrayList<MediaImage>(thumbnails.values());
             }
+
         } else if (checkValidJsonArray(jsonElement)) {
             JsonArray jsonArray = jsonElement.getAsJsonArray();
-            thumbnailType = new TypeToken<List<ImagePost>>() {
+            thumbnailType = new TypeToken<List<MediaImage>>() {
             }.getType();
-            List<ImagePost> imagePostList = gson.fromJson(jsonArray, thumbnailType);
+            List<MediaImage> imagePostList = FoxApplication.getInstance()
+                    .getDataGson()
+                    .fromJson(jsonArray, thumbnailType);
             if (imagePostList != null && imagePostList.size() != 0) {
-                return imagePostList.get(0);
+                return imagePostList;
             }
         }
-        return null;
+        return new ArrayList<>();
     }
 }
