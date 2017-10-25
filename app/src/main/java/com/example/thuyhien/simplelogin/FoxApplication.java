d48fa9c08b54f02cbcf070a2c09c9ff1159b5213
ThuyHien261195 <thuyhien261195@gmail.com>
2017-10-25 17:32:18 +0700

    Move getInstance and Instance field in FoxApplication.java

--- app/src/main/java/com/example/thuyhien/simplelogin/FoxApplication.java
+++ app/src/main/java/com/example/thuyhien/simplelogin/FoxApplication.java
@@ -2,10 +2,9 @@ package com.example.thuyhien.simplelogin;
 
 import android.app.Application;
 
-import com.example.thuyhien.simplelogin.component.AppComponent;
-import com.example.thuyhien.simplelogin.component.DaggerAppComponent;
-import com.example.thuyhien.simplelogin.module.AppModule;
-import com.example.thuyhien.simplelogin.module.NetModule;
+import com.example.thuyhien.simplelogin.dagger.component.AppComponent;
+import com.example.thuyhien.simplelogin.dagger.component.DaggerAppComponent;
+import com.example.thuyhien.simplelogin.dagger.module.AppModule;
 
 /**
  * Created by thuyhien on 10/10/17.
@@ -13,15 +12,10 @@ import com.example.thuyhien.simplelogin.module.NetModule;
 
 public class FoxApplication extends Application {
     public static String langCode = "en";
-    private static FoxApplication instance;
     private AppComponent appComponent;
 
     public AppComponent getAppComponent() {
         return appComponent;
-    }
-
-    public static FoxApplication getInstance() {
-        return instance;
     }
 
     @Override
@@ -30,10 +24,6 @@ public class FoxApplication extends Application {
 
         appComponent = DaggerAppComponent.builder()
                 .appModule(new AppModule(this))
-                .netModule(new NetModule())
                 .build();
-
-        appComponent.inject(this);
-        instance = this;
     }
 }
