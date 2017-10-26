ae149dabf008ee67783173ce817eb16c36a172d4
ThuyHien261195 <thuyhien261195@gmail.com>
2017-10-26 19:20:11 +0700

    Rename FragmentListener Delete Drawable image_poster_bg.xml Add dimension for screen size > 480

--- app/src/main/java/com/example/thuyhien/simplelogin/ui/listener/MainActivityListener.java
+++ app/src/main/java/com/example/thuyhien/simplelogin/ui/listener/MainActivityListener.java
@@ -1,9 +1,11 @@
 package com.example.thuyhien.simplelogin.ui.listener;
 
+import com.example.thuyhien.simplelogin.model.MediaFeed;
+
 /**
  * Created by thuyhien on 10/23/17.
  */
 
-public interface FragmentListerner {
+public interface MainActivityListerner {
     void onChangeTitlePage(String pageId, String title);
 }
