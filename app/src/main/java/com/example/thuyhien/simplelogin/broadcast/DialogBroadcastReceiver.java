c37b97af9e0d8e6f70b7ee7060d573823c79cf92
ThuyHien261195 <thuyhien261195@gmail.com>
2017-10-29 23:54:58 +0700

    Update method onReceive of DialogBroadcastReceiver class to receive intent show feed detail

--- app/src/main/java/com/example/thuyhien/simplelogin/broadcast/DialogBroadcastReceiver.java
+++ app/src/main/java/com/example/thuyhien/simplelogin/broadcast/DialogBroadcastReceiver.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 
 import com.example.thuyhien.simplelogin.R;
 import com.example.thuyhien.simplelogin.model.MediaFeed;
+import com.example.thuyhien.simplelogin.ui.activity.MainActivity;
+import com.example.thuyhien.simplelogin.ui.adapter.PosterAdapter;
 import com.example.thuyhien.simplelogin.ui.listener.MainActivityListener;
 import com.example.thuyhien.simplelogin.ui.viewholder.LandsPosterViewHolder;
 import com.example.thuyhien.simplelogin.ui.viewholder.PosterViewHolder;
@@ -29,26 +31,14 @@ public class DialogBroadcastReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
 
-        if (action.equals(LandsPosterViewHolder.ACTION_SHOW_FEED_DIALOG)) {
-            Bundle bundle = intent.getBundleExtra(PosterViewHolder.BUNDLE_MEDIA_FEED);
-            if (bundle == null) {
+        if (action.equals(MainActivity.ACTION_SHOW_FEED_DETAIL)) {
+            Bundle feedBundle = intent.getBundleExtra(MainActivity.BUNDLE_MEDIA_FEED);
+            if (feedBundle == null) {
                 return;
             }
 
-            MediaFeed mediaFeed = (MediaFeed) bundle.getSerializable(PosterViewHolder.MEDIA_FEED);
-            if (mediaFeed == null) {
-                return;
-            }
-
-            try {
-                if (getMainActivityListener() != null) {
-                    getMainActivityListener().onShowMediaFeedDialog(mediaFeed);
-                }
-            } catch (ClassCastException e) {
-                throw new ClassCastException(context.toString() +
-                        context.getString(R.string.error_implement_main_activity_listener));
-            } catch (IllegalStateException e) {
-                e.printStackTrace();
+            if (getMainActivityListener() != null) {
+                getMainActivityListener().onShowMediaFeedDetail(feedBundle);
             }
         }
     }
