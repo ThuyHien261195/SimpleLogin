ac32b37ae8238baad03abee5d134e3410a6b5360
ThuyHien261195 <thuyhien261195@gmail.com>
2017-11-01 19:00:52 +0700

    Add method getUsedLanguage in FoxApplication

--- app/src/main/java/com/example/thuyhien/simplelogin/FoxApplication.java
+++ app/src/main/java/com/example/thuyhien/simplelogin/FoxApplication.java
@@ -1,18 +1,25 @@
 package com.example.thuyhien.simplelogin;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.example.thuyhien.simplelogin.dagger.component.AppComponent;
 import com.example.thuyhien.simplelogin.dagger.component.DaggerAppComponent;
 import com.example.thuyhien.simplelogin.dagger.module.AppModule;
+import com.example.thuyhien.simplelogin.data.manager.AppManager;
+
+import javax.inject.Inject;
 
 /**
  * Created by thuyhien on 10/10/17.
  */
 
 public class FoxApplication extends Application {
-    public static String langCode = "en";
+    public static String langCode;
     private AppComponent appComponent;
+
+    @Inject
+    AppManager appManager;
 
     public AppComponent getAppComponent() {
         return appComponent;
@@ -25,5 +32,12 @@ public class FoxApplication extends Application {
         appComponent = DaggerAppComponent.builder()
                 .appModule(new AppModule(this))
                 .build();
+        appComponent.inject(this);
+        getUsedLanguage();
+    }
+
+    private void getUsedLanguage() {
+        langCode = appManager.getUsedLanguage();
+        Log.e("LangCode", langCode);
     }
 }
